25cf858a56179b357f366fbc13a1157f2486f4c7
Zoe Rochester <rochester@wustl.edu>
2025-10-01 14:04:38 -0500

    Studio 4 finished

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,24 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		JFileChooser chooser = new JFileChooser("resources");
+		chooser.showOpenDialog(null);
+		File f = new File(chooser.getSelectedFile().getPath());
+		Scanner in = new Scanner(f); //making Scanner with a File
+		double x = .5;
+		double y = .5;
+		double halfWidth = .5;
+		double halfHeight = .25;
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle( x,  y,  halfWidth,  halfHeight);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
+import java.awt.Color;
 
 import edu.princeton.cs.introcs.StdDraw;
 
@@ -15,10 +16,64 @@ import edu.princeton.cs.introcs.StdDraw;
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		JFileChooser chooser = new JFileChooser("resources");
-		chooser.showOpenDialog(null);
-		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		double x = .5;
+		double y = .5;
+		double halfWidth = .8;
+		double halfHeight = .4;
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle( x,  y,  halfWidth,  halfHeight);
+		double radius = .15;
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledCircle(x, y, radius);
+		double h = .5;
+		double k = .25;
+		double halflength = .1;
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledSquare(h, k, halflength);
+
+		double l = .68;
+		double m = .38;
+		double r = .065;
+		StdDraw.setPenColor(Color.MAGENTA);
+		StdDraw.filledCircle(l, m, r);
+
+		double a = .7;
+		double s = .54;
+		StdDraw.setPenColor(Color.PINK);
+		StdDraw.filledCircle(a, s, r);
+
+		double f = .62;
+		double g = .68;
+		StdDraw.setPenColor(Color.MAGENTA);
+		StdDraw.filledCircle(f, g, r);
+
+		double z = .46;
+		double q = .71;
+		StdDraw.setPenColor(Color.PINK);
+		StdDraw.filledCircle(z, q, r);
+
+		double w = .32;
+		double e = .62;
+		StdDraw.setPenColor(Color.MAGENTA);
+		StdDraw.filledCircle(w, e, r);
+
+		double t = .29;
+		double o = .48;
+		StdDraw.setPenColor(Color.PINK);
+		StdDraw.filledCircle(t, o, r);
+
+		double p = .35;
+		double u = .359;
+		StdDraw.setPenColor(Color.MAGENTA);
+		StdDraw.filledCircle(p, u, r);
+
+
+
+
+
+
+
+
 	}
 }
